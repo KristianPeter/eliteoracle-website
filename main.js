63c644034a7d23c3f35c4ab0bd6ad125189f4e82
Kristian Peter <kpeter@eliteoracle.com>
2022-09-13 20:05:56 -0700

    commit 2

--- main.js
+++ main.js
@@ -58,3 +58,12 @@ form.addEventListener("submit", function (e) {
       }, 5000);
     });
 });
+
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open')
+  nav.classList.toggle('flex')
+  nav.classList.toggle('hidden')
+})
